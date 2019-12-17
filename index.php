9bf4a8d991c64a94b4466d89b9547701d53ab891
SOLO360 <habibuyasin9@gmail.com>
2019-12-17 11:47:51 +0300

    touch5

--- index.php
+++ index.php
@@ -5,38 +5,80 @@
 /**
  *
  */
-class Rectangle
-{
-  //declare attributes
-  public $width = 0;
-  public $height = 0;
+// class Rectangle
+// {
+//   //declare attributes
+//   public $width ;
+//   public $height;
+//
+//   //declare method
+//   public function setSize($w='0',$h='0')
+//   {
+//     $this->width = $w;
+//     $this->height = $h;
+//   }
+//
+//   //method to calculate area
+//   public function getArea()
+//   {
+//     return 'The area of the rectangle is '.($this->width * $this->height);
+//   }
+//
+//   //method to calculate perimeter
+//   public function getPerimeter()
+//   {
+//     return 'The perimeter of the rectangle is '.(($this->width + $this->height)*2);
+//   }
+//
+//   //method check if it is a square
+//   public function getSquare()
+//   {
+//     if($this->width == $this->height){
+//       return 'It is a square';
+//     }else {
+//       return 'It is not a square';
+//     }
+//   }
+// }
 
-  //declare method
-  public function setSize($w='0',$h='0')
-  {
-    $this->width = $w;
-    $this->height = $h;
-  }
-
-  //method to calculate area
-  public function getArea()
-  {
-    return 'The area of the rectangle is '.($this->width * $this->height);
-  }
-
-  //method to calculate perimeter
-  public function getPerimeter()
-  {
-    return 'The perimeter of the rectangle is '.(($this->width + $this->height)*2);
-  }
-
-  //method check if it is a square
-  public function getSquare()
-  {
-    if($this->width == $this->height){
-      return 'It is a square';
-    }else {
-      return 'It is not a square';
-    }
-  }
-}
+//--------Program for Calculator
+// /**
+//  *
+//  */
+// class Calculator
+// {
+//   //define Properties
+//   public $num1;
+//   public $num2;
+//
+//   //create method for setting values
+//   public function setValues($num1,$num2)
+//   {
+//     $this->num1 = $num1;
+//     $this->num2 = $num2;
+//   }
+//
+//   //create method for addittion
+//   public function getAddition()
+//   {
+//     return 'The addittion of the two numbers is '.($this->num1 + $this->num2);
+//   }
+//
+//   //create method for multiplication
+//   public function getMult()
+//   {
+//     return 'The multiplication of the two numbers is '.($this->num1 * $this->num2);
+//   }
+//
+//   //create method to divide
+//   function getDiv()
+//   {
+//         if ($this->num2 == 0) {
+//           return 'You can not divide by zero';
+//         }else {
+//           return 'The Division of the two numbers is '.($this->num1 / $this->num2);
+//         }
+//
+//   }
+//
+// }
--- view.php
+++ view.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -8,16 +5,38 @@
     <title></title>
   </head>
   <body>
-      <?php
-      include 'index.php';//include class
 
-      $area = new Rectangle();
-      $area->setSize(5,5);
-      echo $area->getArea();
-      echo '<br>';
-      echo $area->getPerimeter();
-      echo '<br>';
-      echo $area->getSquare();
+      <?php
+      //include class
+      include 'index.php';
+      // $wid = 6;
+      // $len = 5;
+      // //create an object for class rectangle
+      // $area = new Rectangle();
+      // // setting up values
+      // $area->setSize($wid,$len);
+      // echo "<h1> With width of $wid and length of $len <br></h1>" ;
+      // //call methods
+      // echo $area->getArea();
+      // echo '<br>';
+      // echo $area->getPerimeter();
+      // echo '<br>';
+      // echo $area->getSquare();
+      //
+      // //create objects for class  Calculator
+      // $calc =new Calculator();
+      //
+      // //setting values for num1 and $num2
+      // $calc->setValues(4,0);
+      //
+      // //calling the methods
+      // echo $calc -> getAddition();
+      // echo '<br>';
+      // echo $calc -> getMult();
+      // echo '<br>';
+      // echo $calc ->  getDiv();
+
+
        ?>
 
 
